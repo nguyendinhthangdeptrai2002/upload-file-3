dcf935d225cf13ccb7d37d032fece00077194b13
Thắng Nguyễn Đình <95957411+nguyendinhthangdeptrai2002@users.noreply.github.com>
2022-05-01 15:08:52 +0700

    upload lan 3

---
+++ Students.js
@@ -0,0 +1,63 @@
+﻿[
+    {
+        "username": "teonv",
+        "password": "iloveyou",
+        "fullname": "Nguyễn Văn Tèo",
+        "email": "teonv@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "1995-12-21",
+        "schoolfee": "1500000",
+        "marks": "0"
+    },
+    {
+        "username": "pheonv",
+        "password": "iloveyou",
+        "fullname": "Nguyễn Văn Chí Phèo",
+        "email": "pheonv@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "1985-10-11",
+        "schoolfee": "2500000",
+        "marks": "0"
+    },
+    {
+        "username": "nopt",
+        "password": "iloveyou",
+        "fullname": "Phạm Thị Nở",
+        "email": "nopt@fpt.edu.vn",
+        "gender": "false",
+        "birthday": "1993-05-15",
+        "schoolfee": "2000000",
+        "marks": "0"
+    },
+
+    {
+        "username": "thangnv",
+        "password": "iloveyou",
+        "fullname": "Nguyễn Đình Thắng",
+        "email": "thangv@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "2002-12-21",
+        "schoolfee": "1300000",
+        "marks": "0"
+    },
+    {
+        "username": "hahanv",
+        "password": "iloveyou",
+        "fullname": "Nguyễn Văn haha",
+        "email": "aaanv@fpt.edu.vn",
+        "gender": "true",
+        "birthday": "1985-10-11",
+        "schoolfee": "2500000",
+        "marks": "0"
+    },
+    {
+        "username": "rot roi ",
+        "password": "iloveyou",
+        "fullname": "rot roi ne",
+        "email": "okt@fpt.edu.vn",
+        "gender": "false",
+        "birthday": "1993-05-15",
+        "schoolfee": "2000000",
+        "marks": "0"
+    }
+]
---
+++ Subjects.js
@@ -0,0 +1,27 @@
+[
+    {
+        "Id": "ADBS",
+        "Name": "Lập trình Android cơ bản",
+        "Logo": "ADBS.jpg"
+    },
+    {
+        "Id": "DBBS",
+        "Name": "Cơ sở dữ liệu",
+        "Logo": "DBBS.png"
+    },
+    {
+        "Id": "HTCS",
+        "Name": "HTML5 và CSS3",
+        "Logo": "HTCS.jpg"
+    },
+    {
+        "Id": "PHPP",
+        "Name": "Lập trình PHP",
+        "Logo": "PHPP.png"
+    },
+    {
+        "Id": "WEBU",
+        "Name": "Xây dựng trang web",
+        "Logo": "WEBU.jpg"
+    }
+]
